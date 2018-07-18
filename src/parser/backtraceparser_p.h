157184544c2bd821827aae1fd8e25272086be4e5
Laurent Montel <montel@kde.org>
2018-07-18 07:29:27 +0200

    Fix warning

--- src/parser/backtraceparser_p.h
+++ src/parser/backtraceparser_p.h
@@ -24,7 +24,7 @@ class BacktraceParserPrivate
 {
 public:
     BacktraceParserPrivate() : m_usefulness(BacktraceParser::InvalidUsefulness) {}
-    virtual ~BacktraceParserPrivate() {}
+    ~BacktraceParserPrivate() {}
 
     QList<BacktraceLine> m_linesList;
     QList<BacktraceLine> m_linesToRate;
