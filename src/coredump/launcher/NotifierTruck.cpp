aea7570bb7abb7a851bd459c27b3a82d93d4c28b
Harald Sitter <sitter@kde.org>
2021-10-19 15:22:07 +0200

    fix the notifier dump truck
    this broke a tad in the refactor towards standalone launcher. for one
    knotification requires actual eventlooping so the plugin now executes
    the qcoreapplication to get that going.
    
    additionally it no longer uses qprocess to start konsole but KIO's
    terminallauncherjob. this has the advantage of not hardcoding konsole
    and also survives systemd cleaning up. previously the detatched qprocess
    would get killed when we had exited because it was part of our cgroup
    that gets cleaned up by systemd when the launcher unit stops.
    this technically could have used exec() on the job but nested looping is
    always garbage but even more so considering knotification has automatica
    cleanup facilities that we definitely do not want to run while we are in
    a slot. in any event its better to forcefully timeout the start in case
    it gets hung on something anyway.
    
    deprecations have also been bumped ahead to get access to the modern
    activated signal without having to cast away the ambiguity

--- src/coredump/launcher/NotifierTruck.cpp
+++ src/coredump/launcher/NotifierTruck.cpp
@@ -5,44 +5,68 @@
 
 #include "NotifierTruck.h"
 
-#include <KNotification>
+#include <chrono>
+
+#include <QCoreApplication>
 #include <QFile>
 #include <QProcess>
+#include <QTimer>
 
-#include <sys/types.h>
-#include <unistd.h>
+#include <KNotification>
+#include <KTerminalLauncherJob>
 
 #include "../coredump.h"
 
+using namespace std::chrono_literals;
+
 bool NotifyTruck::handle(const Coredump &dump)
 {
+    auto notification = new KNotification(QStringLiteral("applicationcrash"));
+
+    // immediate exit signal. This gets disconnected should `activated` arrive first (in that case we
+    // want to wait for the terminal app to start and not exit on further notification signals)
+    QObject::connect(notification, &KNotification::closed, this, [this, notification] {
+        notification->disconnect(this);
+        qApp->exit(0);
+    });
+
     if (!QFile::exists(dump.filename)) {
-        auto notification = new KNotification(QStringLiteral("applicationcrash"));
         notification->setTitle(QStringLiteral("The warpcore has gone missing"));
         notification->setText(QStringLiteral("%1 [%2] crashed but has no core file").arg(dump.exe, QString::number(dump.pid)));
-        notification->setFlags(KNotification::DefaultEvent | KNotification::SkipGrouping);
-        notification->sendEvent();
-        return true;
+    } else {
+        notification->setTitle(QStringLiteral("He's dead, Jim"));
+        notification->setText(QStringLiteral("%1 [%2]").arg(dump.exe, QString::number(dump.pid)));
+        notification->setActions({QStringLiteral("gdb")});
+
+        const auto pid = dump.pid;
+
+        QObject::connect(notification, &KNotification::activated, notification, [pid, this, notification]() {
+            notification->disconnect(this);
+            auto job = new KTerminalLauncherJob(QStringLiteral("coredumpctl gdb %1").arg(QString::number(pid)), this);
+            connect(job, &KJob::result, this, [job] {
+                if (job->error() != KJob::NoError) {
+                    qWarning() << job->errorText();
+                }
+                qApp->exit(0);
+            });
+            job->start();
+
+            // Just in case the launcher job bugs out also add a timer.
+            auto startTimeout = new QTimer(this);
+            startTimeout->setInterval(16s);
+            connect(startTimeout, &QTimer::timeout, this, [] {
+                qApp->exit(0);
+            });
+            startTimeout->start();
+        });
     }
-
-    auto notification = new KNotification(QStringLiteral("applicationcrash"));
-    notification->setTitle(QStringLiteral("He's dead, Jim"));
-    notification->setText(QStringLiteral("%1 [%2]").arg(dump.exe, QString::number(dump.pid)));
-    notification->setActions({QStringLiteral("gdb")});
-
-    const auto pid = dump.pid;
-    const auto uid = dump.uid;
-    QObject::connect(notification, static_cast<void (KNotification::*)(unsigned int)>(&KNotification::activated), notification, [pid, uid]() {
-        QStringList args{QStringLiteral("--nofork"), QStringLiteral("-e"), QStringLiteral("coredumpctl"), QStringLiteral("gdb"), QString::number(pid)};
-        // if the crash isn't from the current user then route it through pkexec
-        if (uid != getuid()) {
-            args = QStringList{QStringLiteral("--nofork"), QStringLiteral("-e"), QStringLiteral("pkexec coredumpctl gdb %1").arg(pid)};
-        }
-        QProcess::startDetached(QStringLiteral("konsole"), args);
-    });
 
     notification->setFlags(KNotification::DefaultEvent | KNotification::SkipGrouping);
     notification->sendEvent();
 
+    // KNotification internally depends on an eventloop to communicate over dbus. We therefore start the
+    // eventloop here. ::handle() is expected to be blocking so this has no adverse effects.
+    // The eventloop is either exited when the notification gets closed or when the debugger has started.
+    qApp->exec();
     return true;
 }
