caae683ab7bd42d9b74f0c6f459b737c54cf30d4
Harald Sitter <sitter@kde.org>
2024-10-28 14:28:17 +0100

    Messages.sh: add some context to comments
    a fellow developer was thrown off by the spaces comment, when that was
    just explaining the spellcheck disable in the next line

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,9 +2,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # SPDX-FileCopyrightText: 2020-2022 Harald Sitter <sitter@kde.org>
 
+# Ensure passing shellcheck:
 # Our l10n scripting isn't working with spaces anywhere and we actively rely on word splitting in our Messages.sh.
 # shellcheck disable=SC2046
-
 podir=${podir:?} # ensure it is defined
 
 $XGETTEXT $(find . -name \*.cpp -o -name \*.h) -o "$podir"/drkonqi.pot
