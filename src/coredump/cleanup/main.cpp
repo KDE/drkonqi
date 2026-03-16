e274dc077dea0551de515ae992f2be91c0733890
Harald Sitter <sitter@kde.org>
2026-03-16 12:41:09 +0100

    coredump: do not excavate cores to tmp (& add fallback cleanup)
    I was being a bit daft here. By excavating into tmpdir we are possibly
    writing the core to a RAM-backend tmpfs. this is entirely stupid since
    we a) need the RAM for gdb b) when we do this we may run out of RAM
    space and fail to excavate.
    
    instead we now place the core in .cache/drkonqi/cores/
    
    since this has potential of leaking the cleanup util has been adjusted
    to also clean this new directory if it finds too old data inside

--- src/coredump/cleanup/main.cpp
+++ src/coredump/cleanup/main.cpp
@@ -1,17 +1,48 @@
-/*
-    SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-    SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
-*/
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+// SPDX-FileCopyrightText: 2021-2026 Harald Sitter <sitter@kde.org>
 
-#include <cassert>
+#include <algorithm>
 #include <chrono>
 #include <filesystem>
 #include <iostream>
-#include <string_view>
 
 using namespace std::chrono_literals;
 
-int main(int argc, char **argv)
+namespace
+{
+// Heart of the cleanup logic. May throw.
+bool cleanup(const std::filesystem::directory_entry &path)
+{
+    const auto lastModified = path.last_write_time();
+    const auto now = decltype(lastModified)::clock().now();
+    const auto age = now - lastModified;
+    // Plenty of time so we won't take away the file from underneath drkonqi.
+    if (age >= std::chrono::weeks(1)) {
+        std::filesystem::remove_all(path);
+    }
+    return true;
+}
+
+template<typename Precondition>
+bool clean(const auto &path, const Precondition &precondition)
+try {
+    if (!std::filesystem::exists(path)) {
+        return true;
+    }
+    return std::ranges::all_of(std::filesystem::directory_iterator(path), [&](const auto &path) {
+        if (!precondition(path)) {
+            return true;
+        }
+        return cleanup(path);
+    });
+} catch (const std::filesystem::filesystem_error &error) {
+    std::cerr << "Failed to clean: " << path << " " << error.what() << "\n";
+    return false;
+}
+
+} // namespace
+
+int main(int argc, char *argv[])
 {
     if (argc != 2) {
         std::cerr << "Need cache path as argument\n";
@@ -24,21 +55,18 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    for (const auto &path : std::filesystem::directory_iterator(cachePath)) {
-        if (path.path().extension() != ".ini") {
-            continue;
-        }
-        try {
-            const auto lastModified = path.last_write_time();
-            const auto now = decltype(lastModified)::clock().now();
-            const auto age = now - lastModified;
-            // Plenty of time so we won't take away the file from underneath drkonqi.
-            if (age >= std::chrono::weeks(1)) {
-                std::filesystem::remove(path);
-            }
-        } catch (const std::filesystem::filesystem_error &error) {
-            std::cerr << error.what();
-            continue;
-        }
+    auto ret = 0;
+    if (!clean(cachePath / "kcrash-metadata", [](const auto &path) {
+            return path.path().extension() == ".ini";
+        })) {
+        std::cerr << "Failed to clean KCrash metadata\n";
+        ret = 1;
     }
+    if (!clean(cachePath / "drkonqi/cores", [](const auto &path) {
+            return path.is_directory();
+        })) {
+        std::cerr << "Failed to clean cores\n";
+        ret = 1;
+    }
+    return ret;
 }
