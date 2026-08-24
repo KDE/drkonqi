621e53262faa720aa8a17e3c30aadca4f3c6848f
Harald Sitter <sitter@kde.org>
2026-08-24 09:07:39 +0200

    launcher.service: ensure it doesn't participate in session management
    (on x11)
    
    BUG: 524213

--- src/coredump/launcher/drkonqi-coredump-launcher@.service.cmake
+++ src/coredump/launcher/drkonqi-coredump-launcher@.service.cmake
@@ -8,6 +8,8 @@ Requisite=graphical-session.target
 ConditionUser=!@system
 
 [Service]
+# Make extra sure the launcher is not part of session management. It only works when started from systemd!
+Environment=SESSION_MANAGER=
 # Don't need to be working anywhere specific, use tmp.
 WorkingDirectory=%T
 ExecStart=@KDE_INSTALL_FULL_LIBEXECDIR@/drkonqi-coredump-launcher
