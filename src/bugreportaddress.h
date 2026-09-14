fbef4ac3e19ca44929ff8d6d80aa04f9bf0f541c
Harald Sitter <sitter@kde.org>
2026-09-14 13:22:41 +0200

    bugreportaddress: make KDE detection more flexible
    previously we only allowed the default value. at some point the
    semantics of the property was changed to also allow URLs and now some
    apps are setting it to random URLs. try our best to figure out if the
    URL points to KDE infrastructure and accept reports for it
    
    add a test while at it

--- src/bugreportaddress.h
+++ src/bugreportaddress.h
@@ -1,5 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2009 George Kiagiadakis <gkiagia@users.sourceforge.net>
+    SPDX-FileCopyrightText: 2026 Harald Sitter <sitter@kde.org>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
@@ -7,7 +8,9 @@
 #define BUGREPORTADDRESS_H
 
 #include <QString>
+#include <QUrl>
 
+#include "drkonqi_debug.h"
 #include "drkonqi_globals.h"
 
 class BugReportAddress : public QString
@@ -18,7 +21,7 @@ public:
     {
     }
     inline BugReportAddress(const QString &address)
-        : QString(address == QLatin1String("submit@bugs.kde.org") ? KDE_BUGZILLA_URL : address)
+        : QString(looksLikeKDE(address) ? KDE_BUGZILLA_URL : address)
     {
     }
 
@@ -30,6 +33,28 @@ public:
     inline bool isEmail() const
     {
         return contains(QLatin1Char('@'));
+    }
+
+private:
+    [[nodiscard]] static bool looksLikeKDE(const QString &address)
+    {
+        // The default value is a raw email address from the days of yore. Special case it since it is not a URI.
+        if (address == QLatin1String("submit@bugs.kde.org")) {
+            return true;
+        }
+
+        // In all other cases we expect a URI and check that it points to something vaguely KDE.
+        QString host = [&] {
+            QUrl url(address, QUrl::StrictMode);
+            if (!url.isValid()) {
+                qCWarning(DRKONQI_LOG) << "Invalid URI" << address;
+                return QString();
+            }
+            // URIs (e.g. mailto:) will parse into scheme and path only. Path will also parse fragments though so it should be clean™
+            return url.host().isEmpty() ? url.path() : url.host();
+        }();
+        // Cover all bases here: bugs.kde.org, bugstest.kde.org, invent.kde.org...
+        return host.endsWith(QLatin1String(".kde.org"));
     }
 };
 
