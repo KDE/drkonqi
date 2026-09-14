4377feecca6667f3f80c9c8fda5f6f9f20859b63
Harald Sitter <sitter@kde.org>
2026-09-14 12:43:55 +0200

    globals: trim
    now that the qwidget code base is gone we don't need most of it anymore
    
    (cherry picked from commit 69c183996e3757d993285d7aa0ba3231427e4546)

--- src/drkonqi_globals.h
+++ src/drkonqi_globals.h
@@ -6,32 +6,9 @@
 #ifndef DRKONQI_GLOBALS_H
 #define DRKONQI_GLOBALS_H
 
-#include <KGuiItem>
-#include <QIcon>
-
 #include "drkonqi.h"
-
-/** This class provides a custom constructor to fill the "toolTip"
- * and "whatsThis" texts of KGuiItem with the same text.
- */
-class KGuiItem2 : public KGuiItem
-{
-public:
-    inline KGuiItem2(const QString &text, const QIcon &icon, const QString &toolTip)
-        : KGuiItem(text, icon, toolTip, toolTip)
-    {
-    }
-};
-
-namespace DrStandardGuiItem
-{
-KGuiItem2 appRestart();
-}; // namespace DrStandardGuiItem
 
 /* Urls are defined globally here, so that they can change easily */
 #define KDE_BUGZILLA_URL DrKonqi::kdeBugzillaURL()
-#define KDE_BUGZILLA_CREATE_ACCOUNT_URL KDE_BUGZILLA_URL + QStringLiteral("createaccount.cgi")
-#define KDE_BUGZILLA_SHORT_URL "bugs.kde.org"
-#define TECHBASE_HOWTO_DOC "https://community.kde.org/Guidelines_and_HOWTOs/Debugging/How_to_create_useful_crash_reports#Preparing_your_KDE_packages"
 
 #endif
